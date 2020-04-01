ef3dec2506ce1ae529f839f3ddf3f052d88bc20b
eeroaaremaa <eero.aaremaa@gmail.com>
2020-04-01 23:32:37 +0300

    Kommentaarid lisatud

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 
 public class Main {
 
+    //piirangudBoolean hoiab kasutaja poolt sisestatud piiranguid toitumise osas
+    //järjekorras vegan, gluteenivaba ja laktoosivaba
     public static String[] piirangudBoolean = {"false", "false", "false"};
 
     public static void main(String[] args) throws Exception {
+        //Palume kasutajal sisestada enda toitumisega seotud piirangud
         Scanner scanner = new Scanner(System.in);
         System.out.println("Tere tulemast menüü loojasse.");
         System.out.println("Palun sisesta oma toitumispiirangud: ");
         boolean valitud = false;
         ArrayList<String> piirangud = new ArrayList<>();
 
+        //Järgnev while tsükkel küsib kasutajalt tema toitumispiiranguid, kuni
+        //ta kõik korrektselt sisestanud on
         while (!valitud) {
             System.out.println("Kui oled vegan sisesta V. Kui oled gluteenivaba sisesta G. Kui oled laktoosivaba sisesta L. Kui sul piiranguid ei ole siis vajuta X");
             String valik = scanner.next();
@@ -27,6 +32,8 @@ public class Main {
                 System.out.println("Palun sisesta sobiv varinat");
                 continue;
             }
+
+            //Kui kasutaja on piirangud ära sisestanud siis liigume järgmise osa juurde
             System.out.println("Kui oled oma valikud ära valinud siis sisesta X, kui soovid veel piiranguid lisada sisesta N.");
             valik = scanner.next();
             if (valik.equals("X")) {
@@ -38,7 +45,7 @@ public class Main {
             }
         }
 
-        //V, G, L
+        //viime piirangudBooleani andmed sisse sõnedena
         for (int i = 0; i < 3; i++) {
             try {
                 if (piirangud.get(i).equals("G")) {
@@ -52,10 +59,13 @@ public class Main {
             }
         }
 
+        //Loeme failist toidud
         Toidud toidud = new Toidud();
         toidud.failistLugemine("toidud.txt");
-        System.out.println(toidud.getToidud());
 
+        //Järgen while loop küsib kasutajalt mida ta järgmisena teha soovib
+        //Kasutaja saab küsida programmilt järgmist suvalist toitu, et kasutaja
+        //ei peaks ise valima mis toitu ta järgmisena süüa soovib.
         while (true) {
             System.out.println("Järgmise toidu nägemiseks sisesta J, toidu lisamiseks sisesta +, programmist lahkumiseks sisesta X");
             String valik = scanner.next();
@@ -65,6 +75,7 @@ public class Main {
                 System.out.println("Toidu lisamiseks sisesta toit järgmise vormistusega:");
                 String[] sisestamiseleA = new String[4];
 
+                //Kastaja peab sisestama toidu nime ja toitumispiirangute info
                 System.out.println("Toidu nimi: ");
                 String sisestamisele = scanner.next();
                 sisestamiseleA[0] = sisestamisele;
@@ -96,6 +107,8 @@ public class Main {
                     continue;
                 }
 
+                //Kui kasutaja on toidu andmed sisestanud siis lisame selle ka faili
+
                 String faili = sisestamiseleA[0] + ", " + sisestamiseleA[1] + ", " + sisestamiseleA[2] + ", " + sisestamiseleA[3];
 
                 File file = new File("toidud.txt");
@@ -103,13 +116,13 @@ public class Main {
                 fileWriter.write("\n" + faili);
                 fileWriter.close();
 
-                System.out.println(sisestamiseleA[0] + "  " + sisestamiseleA[1] + "  " + sisestamiseleA[2] + "  " + sisestamiseleA[3]);
+                //Kui lisatud toit sobib tema toitumispiirangutega lisa see toitude nimekirja
                 if (Toidud.sobivus(sisestamiseleA)) {
                     toidud.lisaToit(sisestamiseleA[0], Toidud.leiaVäärtus(sisestamiseleA[1]),
                             Toidud.leiaVäärtus(sisestamiseleA[2]), Toidud.leiaVäärtus(sisestamiseleA[3]));
                     continue;
                 }
-                System.out.println(toidud.getToidud());
+            //Kui kasutaja valib X siis pannakse kogu programm kinni.
             } else if (valik.equals("X")) {
                 System.exit(0);
             }
@@ -117,6 +130,7 @@ public class Main {
 
     }
 
+    //Järgnev meetod valib juhusliku toidu listist ning tagastab selle sõnena
     static String juhuslikGenereerija(Toidud toidud) {
         return toidud.getToidud().get((int) (Math.random() * toidud.getPikkus()) + 1).toString();
     }
